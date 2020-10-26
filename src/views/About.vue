407b9ddd2805bd31a2746efca1d46ca90c1c5de9
Bruno Francisco <bruno11.francisco@gmail.com>
2020-10-26 10:47:58 +0100

    init

---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>
+      This is the Foreo VueJS project. Please, add all your code in the
+      <router-link to="/">main page</router-link>
+    </h1>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <!--  @todo: add your code here  -->
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {},
+})
+export default class Home extends Vue {}
+</script>
